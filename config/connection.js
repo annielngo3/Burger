4fae306858bcc5abeed5fcd41a868dc09f5e443d
annielngo3 <annielngo3@gmail.com>
2018-12-06 19:10:31 -0600

    added JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,27 @@
+// dependencies
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// uses JawsDB to configure connection
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+		user: "root",
+		password: "root",
+		database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+	if (err) {
+		console.log("error connecting: " + err.stack);
+		return;
+	}
+
+	console.log("connected as id: " + connection.threadId);
 });
 
 module.exports = connection;
